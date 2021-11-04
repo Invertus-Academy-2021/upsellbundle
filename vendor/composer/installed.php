48690e43d60ec177cd0452ba50377a61238cea03
mandan2 <61560082+mandan2@users.noreply.github.com>
2021-11-04 19:35:53 +0200

    Composer autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '685bb74d8a3fdcc41643265f79a91bd91399974a',
+        'name' => 'gg/upsellbundle',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gg/upsellbundle' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '685bb74d8a3fdcc41643265f79a91bd91399974a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
